a335d581169b755a47502b127f767b2daf731029
SteveOcappie <steveocappie@gmail.com>
2018-11-06 17:59:20 -0600

    Fixed compiler errors

--- cmd_parse.c
+++ cmd_parse.c
@@ -28,18 +28,23 @@ struct parsedCMD* parseCMD(int argc, char** argv){
 					fprintf(stderr, "File length is too long, exiting");
 					exit(-1);
 				}
+				break;
 			case '?':
 				if(optopt == 'f'){
 					fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+					break;
 				}
 				else if(isprint(optopt)){
 					fprintf(stderr, "Unknown option -%c. \n", optopt);
+					break;
 				}
 				else{
 					fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
+					break;
 				}
 			default:
 				abort();
+				break;
 		}
 	}
 	// take the first arg as the specified rule
